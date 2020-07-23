47704b29122bc3bd43a0389dfc3b88bfd65e91e6
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-23 01:25:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ end
 
 def list_dwarves(array)
   
-  array.each_with_index
+  array.each_with_index do |
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
